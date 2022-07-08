df118d917d8f3284f79e92a3c5fb98bcfcab4b99
Jake Riordan <contact@jakeriordan.dev>
2022-07-08 15:31:28 +0200

    init(env): add data fetching packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import logo from "./logo.svg";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="text-3xl font-bold underline">
+            <Example />
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+function Example() {
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+      (res) => res.json()
+    )
+  );
+
+  if (isLoading) return <p>"Loading..."</p>;
+
+  if (error instanceof Error && error.message)
+    return <p>An error has occurred: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <br />
+      <p>{data.description}</p>
+      <br />
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
     </div>
   );
 }
