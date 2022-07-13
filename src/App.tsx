89f8ba5baea5ac959ea34ffe1a3ccb1949974524
Jake Riordan <contact@jakeriordan.dev>
2022-07-13 20:20:23 +0200

    feat(app): setup app for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,16 @@
-import React from "react";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import logo from "./logo.svg";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
+import TagContainer from "./components/TagContainer";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({});
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="text-3xl font-bold underline">
-            <Example />
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App p-8">
+        <TagContainer />
       </div>
     </QueryClientProvider>
-  );
-}
-
-function Example() {
-  const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
-      (res) => res.json()
-    )
-  );
-
-  if (isLoading) return <p>"Loading..."</p>;
-
-  if (error instanceof Error && error.message)
-    return <p>An error has occurred: {error.message}</p>;
-
-  return (
-    <div>
-      <h1>{data.name}</h1>
-      <br />
-      <p>{data.description}</p>
-      <br />
-      <strong>👀 {data.subscribers_count}</strong>{" "}
-      <strong>✨ {data.stargazers_count}</strong>{" "}
-      <strong>🍴 {data.forks_count}</strong>
-    </div>
   );
 }
 
