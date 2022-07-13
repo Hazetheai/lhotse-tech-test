ec13fa1a1a60d59f1b20d27349ac852d96a58017
Jake Riordan <contact@jakeriordan.dev>
2022-07-13 20:22:28 +0200

    feat(comps): init base for data fetching

---
+++ src/components/TagContainer.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import { useQuery } from "react-query";
+import TagDisplay from "./TagDisplay";
+
+const TagContainer: FC = () => {
+  const { isLoading, error, data } = useQuery(
+    "supplier",
+    async () => {
+      const _res = await fetch(
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000/api/supplier"
+          : `${process.env.VERCEL_URL}/api/supplier`
+      );
+      const res = await _res.json();
+
+      return JSON.parse(res.body);
+    },
+    // Unnecessary due to static data source
+    { refetchOnWindowFocus: false }
+  );
+
+  if (isLoading) return <p>"Loading App..."</p>;
+
+  if (error instanceof Error && error.message) {
+    return <p>An error has occurred: {error.message}</p>;
+  }
+
+  return <div>{data && <TagDisplay supplier={data} />}</div>;
+};
+
+export default TagContainer;
